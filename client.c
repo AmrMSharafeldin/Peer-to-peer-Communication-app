8c373bc50cb51a9b81e78a5e5527754e8a018477
AmrMohamedSharaf <ams47@sfu.ca>
2023-02-18 03:05:54 +0000

    server

--- client.c
+++ client.c
@@ -11,7 +11,7 @@ int main(int argc, char const* argv[])
 {
 	int sock = 0, valread, client_fd;
 	struct sockaddr_in serv_addr;
-	char* hello = "Slav Fuck you!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1";
+	char* hello = "Wanna go out !!!! ";
 	char buffer[1024] = { 0 };
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		printf("\n Socket creation error \n");
