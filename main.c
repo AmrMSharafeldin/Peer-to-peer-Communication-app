4515dd2432ea0924d6c79fb1e3351ccdecae991a
sra124 <sra124@sfu.ca>
2023-02-17 18:57:33 -0800

    client

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ int main()
         exit(EXIT_FAILURE);
     }
 
-    printf("%d", server_fd);
+
 // Forcefully attaching socket to the port 8080
     if (setsockopt(server_fd, SOL_SOCKET,
                    SO_REUSEADDR | SO_REUSEPORT, &opt,
