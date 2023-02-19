1fa80ae452ec9c268db4392ff09cc7e183e35abb
sra124 <sra124@sfu.ca>
2023-02-18 18:13:31 -0800

    Issue #1 sender and receiver are not pairing

--- receiver.c
+++ receiver.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
-#define PORT 6969
+#define PORT 8080
 #define MAX_LEN 1024
 #define ID_ADDR "127.0.0.1"
 //static int sock;
@@ -14,9 +14,9 @@ int init_socket()
     struct sockaddr_in sin;
     memset(&sin, 0, sizeof(sin));
     sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = htonl(INADDR_ANY);
+    sin.sin_addr.s_addr = htonl("127.0.0.1");
     sin.sin_port = htons(PORT);
-    int socketDescriptor = socket(PF_INET, SOCK_DGRAM, 0);
+    int socketDescriptor = socket(AF_INET, SOCK_DGRAM, 0);
 
     bind(socketDescriptor, (struct sockaddr* ) &sin, sizeof(sin));
     return socketDescriptor;
@@ -29,7 +29,7 @@ void lis(int sock)
         struct sockaddr_in sinRemote;
         unsigned int sin_len = sizeof(sinRemote);
         char messageRx[MAX_LEN];
-        int bytesRx = recvfrom(sock, messageRx, MAX_LEN, 0, (struct sockaddr*) &sinRemote, &sin_len);
+        int bytesRx = recvfrom(sock, messageRx, MAX_LEN, 0, (struct sockaddr_in*) &sinRemote, &sin_len);
         
         int terminateIdx = (bytesRx < MAX_LEN) ? bytesRx : MAX_LEN -1;
         printf("Message received (%d): \n%s \n", bytesRx, messageRx);
--- sender.c
+++ sender.c
@@ -5,41 +5,25 @@
 #include <stdlib.h>
 #include <netdb.h>
 #include <unistd.h>
-#define PORT 6969
+#define PORT 8080
 #define MAX_LEN 1024
 // Socket address 
 
 
-
-
-
-int init_sokcet_connect(){ 
- 
-}
-
 int main(){
        // create the socket addresss 
-    struct sockaddr_in sin; 
-    sin.sin_addr.s_addr = htonl(INADDR_ANY);
-    sin.sin_family = AF_INET; 
-    sin.sin_port = htons(PORT);
+   struct sockaddr_in sin; 
+   sin.sin_addr.s_addr = htonl("127.0.0.1");
+   sin.sin_family = AF_INET; 
+   sin.sin_port = htons(PORT);
 
    int  sock; 
-    sock = socket(AF_INET , SOCK_STREAM , 0 ) ;
-   if(sock == -1){printf("Failed");
-        return -1 ;
-   }
+   sock = socket(AF_INET , SOCK_STREAM , 0 ) ;
+   char* message = "test";
+   connect(sock, (struct sockaddr_in*)&sin,sizeof(sin)) ;
+   send(sock , message , strlen(message) , 0);
 
-    char* message = "test";
-    if ((connect(sock, (struct sockaddr_in*)&sin,sizeof(sin)))< 0){return -1;  }
-    printf("conncected");
-   return sock ;
-	if( send(sock , message , strlen(message) , 0) < 0)
-	{
-		printf("Send failed");
-		return 1;
-	}
-	printf("Data Send\n");
+
 	
 	return 0;
 }
