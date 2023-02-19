602dc39dc2e84bb7e3bef0e99b2836e6974a24f2
sra124 <sra124@sfu.ca>
2023-02-18 19:25:23 -0800

    Issue #1 sender and receiver are not pairing

--- receiver.c
+++ receiver.c
@@ -11,38 +11,68 @@
 //static int sock;
 int init_socket()
 {
+    // struct sockaddr_in sin;
+    // memset(&sin, 0, sizeof(sin));
+    // sin.sin_family = AF_INET;
+    // sin.sin_addr.s_addr = htonl("127.0.0.1");
+    // sin.sin_port = htons(PORT);
+    // int socketDescriptor = socket(PF_INET, SOCK_DGRAM, 0);
+
+    // bind(socketDescriptor, (struct sockaddr* ) &sin, sizeof(sin));
+    //return socketDescriptor;
+}
+
+void lis(int sock)
+{
+    // while(1)
+    // {
+    //     struct sockaddr_in sinRemote;
+    //     unsigned int sin_len = sizeof(sinRemote);
+    //     char messageRx[MAX_LEN];
+    //     int bytesRx = recvfrom(sock, messageRx, MAX_LEN, 0, (struct sockaddr_in*) &sinRemote, &sin_len);
+        
+    //     int terminateIdx = (bytesRx < MAX_LEN) ? bytesRx : MAX_LEN -1;
+    //     printf("Message received (%d): \n%s \n", bytesRx, messageRx);
+
+    //     //int incMe = atoi(messageRx);
+
+        
+    // }
+}
+
+int main()
+{
     struct sockaddr_in sin;
     memset(&sin, 0, sizeof(sin));
     sin.sin_family = AF_INET;
     sin.sin_addr.s_addr = htonl("127.0.0.1");
     sin.sin_port = htons(PORT);
-    int socketDescriptor = socket(AF_INET, SOCK_DGRAM, 0);
+    int socketDescriptor = socket(PF_INET, SOCK_DGRAM, 0);
 
     bind(socketDescriptor, (struct sockaddr* ) &sin, sizeof(sin));
-    return socketDescriptor;
-}
-
-void lis(int sock)
-{
-    while(1)
-    {
-        struct sockaddr_in sinRemote;
-        unsigned int sin_len = sizeof(sinRemote);
+    while(1){
+        fflush(stdout);
+        struct sockaddr_in sinR;
+        int sin_len = sizeof(sinR);
         char messageRx[MAX_LEN];
-        int bytesRx = recvfrom(sock, messageRx, MAX_LEN, 0, (struct sockaddr_in*) &sinRemote, &sin_len);
         
-        int terminateIdx = (bytesRx < MAX_LEN) ? bytesRx : MAX_LEN -1;
-        printf("Message received (%d): \n%s \n", bytesRx, messageRx);
+        listen(socketDescriptor, 10);
+        int sck = sizeof(struct sockaddr_in);
+        int client = accept(socketDescriptor, (struct sockaddr* )& sinR, (socklen_t*)& sck );
+        int size = recv(client, messageRx, MAX_LEN, 0);
 
-        //int incMe = atoi(messageRx);
-
+       
+        int terminateIdx = (size < MAX_LEN) ? size : MAX_LEN -1;
+        printf("Message received: \n%s \n", messageRx);
+        
+        
+        
         
     }
-}
+        //int incMe = atoi(messageRx);
 
-int main()
-{
-    int sock = init_socket();
-    lis(sock);
+    // int sock = init_socket();
+    // printf("%d", sock);
+    // lis(sock);
 
 }
--- sender.c
+++ sender.c
@@ -13,12 +13,12 @@
 int main(){
        // create the socket addresss 
    struct sockaddr_in sin; 
-   sin.sin_addr.s_addr = htonl("127.0.0.1");
+   sin.sin_addr.s_addr = htonl(INADDR_ANY);
    sin.sin_family = AF_INET; 
    sin.sin_port = htons(PORT);
 
    int  sock; 
-   sock = socket(AF_INET , SOCK_STREAM , 0 ) ;
+   sock = socket(AF_INET , SOCK_DGRAM , 0 ) ;
    char* message = "test";
    connect(sock, (struct sockaddr_in*)&sin,sizeof(sin)) ;
    send(sock , message , strlen(message) , 0);
